fd383cbf873fecc88b9859db6b3ababcded37bbd
younes-qtyab <younes.speedyy@gmail.com>
2022-11-02 22:39:43 +0100

    DB HOST EDITEd

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./Controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'younes',
-      database : 'recognition'
+      connectionString : process.env.DATABASE_URL,
+      ssl:true,
     }
   });
 
